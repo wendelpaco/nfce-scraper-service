050320812f093a12c1d50082f52be591f5095e8d
Wendel Santos <wendelpaco@gmail.com>
2025-06-24 22:42:28 -0300

    Initial commit

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 /* eslint-disable no-console */
 // src/server.ts
 import app from "./app";
+import * as dotenv from "dotenv";
+dotenv.config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
